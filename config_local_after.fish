fbb3d408f842ef22b5e75c36c2cbaa27333c0a93
Wang Tiaoke <wangtiaoke@cqsoftware.com.cn>
2024-08-05 14:29:51 +0800

    Added pon function

--- config_local_after.fish
+++ config_local_after.fish
@@ -1,3 +1,9 @@
-if ! set -q TMUX
-    exec tmux
+# if ! set -q TMUX
+#     exec tmux
+# end
+fish_add_path -gp "$HOME/.neovim/bin"
+
+function pon
+    export http_proxy='http://192.168.5.102:7897'
+    export https_proxy='http://192.168.5.102:7897'
 end
